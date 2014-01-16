c902fa00933cdc7e5ac27c1b14f2da779bdb91d2
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-01-16 11:21:14 +0100

    Fix git_version fact

--- lib/facter/git_version.rb
+++ lib/facter/git_version.rb
@@ -1,4 +1,10 @@
 # git_version
-Facter.add('git_version') do
-  setcode 'git --version 2>/dev/null'.sub(/git version /, '')
+output = %x{git --version 2>&1}
+
+if $?.exitstatus and output.match(/git version ((\d+\.){2,}\d+).*/)
+  Facter.add('git_version') do
+    setcode do
+      $1
+    end
+  end
 end
