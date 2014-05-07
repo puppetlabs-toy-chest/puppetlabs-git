eed89a4b91d61e6ea0ccc9636a7c02b582e98b07
Matt Janssen <github@mattjanssen.com>
2014-05-07 18:01:39 -0500

    Added Class['git'] requirement to git::config
    Does it make sense to require git before executing the git command?
    
    Without this, I am getting "Could not find command 'git'" the first time I provision. The second time I provision, it works.
    
    Do I need to put a require statement on every single call to git::config, or can we add it here?

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,6 +4,7 @@ define git::config(
   $key      = regsubst($name, '^([^\.]+)\.([^\.]+)$','\2'),
 ) {
   exec{"git config --global ${section}.${key} '${value}'":
+    require     => Class['git'],
     environment => inline_template('<%= "HOME=" + ENV["HOME"] %>'),
     path        => ['/usr/bin', '/bin'],
     unless      => "git config --global --get ${section}.${key} '${value}'",
