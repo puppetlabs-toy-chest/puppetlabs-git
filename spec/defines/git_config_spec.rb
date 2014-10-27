0b146167890abe5238629d2456b5c8b51f35afda
Peter Souter <p.souter@kainos.com>
2014-10-27 23:56:17 +0000

    Changes to specs for new git_config provider

--- spec/defines/git_config_spec.rb
+++ spec/defines/git_config_spec.rb
@@ -1,5 +1,39 @@
 require 'spec_helper'
 
 describe 'git::config', :type => :define do
-  skip "Needs new specs now provider has changed"
+  context 'has working default parameters' do
+    let(:title) { 'user.name' }
+    let(:params) {
+      {
+        :value => 'JC Denton',
+      }
+    }
+    it do
+      should contain_git_config('user.name').with(
+        'value'   => 'JC Denton',
+        'section' => 'user',
+        'key'     => 'name',
+        'user'    => 'root',
+      )
+      have_git_config_resource_count(1)
+    end
+  end
+  context 'allows you to change user' do
+    let(:title) { 'user.email' }
+    let(:params) {
+      {
+        :value => 'jcdenton@UNATCO.com',
+        :user  => 'admin'
+      }
+    }
+    it do
+      should contain_git_config('user.email').with(
+        'value'   => 'jcdenton@UNATCO.com',
+        'section' => 'user',
+        'key'     => 'email',
+        'user'    => 'admin',
+      )
+      have_git_config_resource_count(1)
+    end
+  end
 end
