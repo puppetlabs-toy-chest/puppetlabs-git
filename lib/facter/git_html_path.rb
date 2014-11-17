7caa0c140807746ca19022d87d0cc347634fc949
Ben Ford <ben.ford@puppetlabs.com>
2014-11-17 12:37:19 -0800

    Don't spit errors on windows
    The stderr redirection to `/dev/null` makes windows emit errors on every
    single facter/puppet run. This simply redirects to `nul` instead when
    running on windows family.

--- lib/facter/git_html_path.rb
+++ lib/facter/git_html_path.rb
@@ -1,4 +1,10 @@
 # git_html_path.rb
 Facter.add('git_html_path') do
-  setcode 'git --html-path 2>/dev/null'
+  case Facter.value(:osfamily)
+  when 'windows'
+    setcode 'git --html-path 2>nul'
+  else
+    setcode 'git --html-path 2>/dev/null'
+  end
 end
+
