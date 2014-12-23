156dfc9531f3c57a5a699e026b4d09cb37a82118
Peter Souter <p.souter@kainos.com>
2014-12-23 16:02:10 +0000

    Fixes fact when git not present
    ```
    Could not retrieve fact='git_version', resolution='<anonymous>': undefined method `strip' for nil:NilClass
    ```

--- lib/facter/git_version.rb
+++ lib/facter/git_version.rb
@@ -14,7 +14,7 @@ Facter.add('git_version') do
   git_version_cmd = 'git --version 2>&1'
   git_version_result = Facter::Util::Resolution.exec(git_version_cmd)
   setcode do
-    git_version_result.to_s.lines.first.strip.split(/version/)[1].strip
+    git_version_result.to_s.lines.first.strip.split(/version/)[1].strip unless git_version_result.nil?
   end
 end
 
