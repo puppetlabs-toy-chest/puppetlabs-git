3c5b15182cd376f2beb215789bb23b27dd1ca2a6
Peter Souter <peter.souter@puppetlabs.com>
2015-01-08 08:38:56 -0800

    (MODULES-1238) Add which to check git present
    As recommended by @elyscape, much cleaner way of only getting the fact if git is found.

--- lib/facter/git_version.rb
+++ lib/facter/git_version.rb
@@ -10,11 +10,12 @@
 #
 # Notes:
 #   None
-Facter.add('git_version') do
-  git_version_cmd = 'git --version 2>&1'
-  git_version_result = Facter::Util::Resolution.exec(git_version_cmd)
-  setcode do
-    git_version_result.to_s.lines.first.strip.split(/version/)[1].strip
+if Facter::Util::Resolution.which('git')
+  Facter.add('git_version') do
+    git_version_cmd = 'git --version 2>&1'
+    git_version_result = Facter::Util::Resolution.exec(git_version_cmd)
+    setcode do
+      git_version_result.to_s.lines.first.strip.split(/version/)[1].strip
+    end
   end
 end
-
