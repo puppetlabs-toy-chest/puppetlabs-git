60c4f3d8d4218cb1e64577557215906b4df5a431
Morgan Haskel <morgan@puppetlabs.com>
2015-02-26 15:51:02 -0800

    Adds docs and test

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,6 +10,9 @@
 #
 # === Parameters
 #
+# [*package_ensure*]
+#   Value to be passed to ensure in the package resource. Defaults to installed.
+#
 # [*package_manage*]
 #   boolean toggle to overide the management of the git package.
 #   You may want to change this behavior if another module manages git packages
