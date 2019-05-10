c75628c594088aa6e44f7025d261edd42d14f345
Siebrand Mazeland <siebrand@kitano.nl>
2019-05-10 15:11:08 +0200

    Avoid duplicate declarations of Package['git']
    Use ensure_packages() to install/remove the package, instead of package{} to avoid
    duplicate declaration of Package['git'].

--- manifests/init.pp
+++ manifests/init.pp
@@ -34,9 +34,7 @@ class git (
   $configs_defaults = {}
 ) {
   if ( $package_manage ) {
-    package { $package_name:
-      ensure => $package_ensure,
-    }
+    ensure_packages([$package_name], { ensure => $package_ensure })
   }
 
   create_resources(git::config, git_config_hash($configs), $configs_defaults)
